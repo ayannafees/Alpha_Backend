1fa4cac6efa73a938cda9a93228e605a83ef88d1
Ayan Nafees <ayan014iiitm@gmail.com>
2024-05-25 21:02:48 +0530

    Fixed register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,9 +15,7 @@ const registerUser = asyncHandler( async (req, res) => {
     // check for user creation
     // return res
 
-
     const {fullName, email, username, password } = req.body
-    //console.log("email: ", email);
 
     if (
         [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -32,10 +30,9 @@ const registerUser = asyncHandler( async (req, res) => {
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exists")
     }
-    //console.log(req.files);
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
 
     // let coverImageLocalPath;
     // if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
